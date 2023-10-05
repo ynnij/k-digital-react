39061f8d732a82cd9709a2222b66575aa634045c
ynnij <ynnnij@gmail.com>
2023-10-05 13:07:23 +0900

    1005 수업 내용 추가

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import MyDiv from './03/MyDiv';
 import Box from './04/Box';
 import Frcst from './05/Frcst';
 import Taccident from './06/Taccident';
+import MyRef from './07/MyRef';
 function App() {
   return (
-    //<Clock />
+    // <Clock />
     // <Lotto />
     // <MyCom />
-    //<MyDiv />
-    // <Box />
-    //<Frcst />
-    <Taccident />
+    // <MyDiv />
+    <Box />
+    // <Frcst />
+    //<Taccident />
+    //<MyRef />
   );
 }
 
