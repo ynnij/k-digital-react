d58694906ea46d7bff81fd7fff7e3186c3506248
ynnij <ynnnij@gmail.com>
2023-10-24 12:56:06 +0900

    공통 컴포넌트 추가 및 수정
    - ButtonBlue 높이 수정
    - TailH1 컴포넌트 추가

--- src/common/ButtonBlue.js
+++ src/common/ButtonBlue.js
@@ -1,7 +1,7 @@
 
 function ButtonBlue(props) {
     return (
-        <button onClick={props.handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded">
+        <button onClick={props.handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0 px-4 rounded h-10">
             {props.caption}
         </button>
     )
---
+++ src/common/TailH1.js
@@ -0,0 +1,9 @@
+const TailH1 = (props) => {
+  return (
+    <h1 className="text-2xl font-bold mb-5">
+        {props.title}
+    </h1>
+  )
+}
+
+export default TailH1
